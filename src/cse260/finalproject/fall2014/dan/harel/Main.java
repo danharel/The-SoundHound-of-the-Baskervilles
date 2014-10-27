86746794c0a25382e0d3cf54874c52deddfc8ad4
danharel <cheezzywizz@gmail.com>
2014-10-27 00:35:14 -0400

    Forgot to add accessor value for variables in Main.

--- src/cse260/finalproject/fall2014/dan/harel/Main.java
+++ src/cse260/finalproject/fall2014/dan/harel/Main.java
@@ -8,10 +8,10 @@ package cse260.finalproject.fall2014.dan.harel;
 public class Main {
 
 	/** Application that identifies a clip */
-	Identifier identifier;
+	private Identifier identifier;
 	
 	/** Application that handles the indexing of a song and its probes */
-	Indexer indexer;
+	private Indexer indexer;
 	
 	/**
 	 * Gives user the option to use the indexing application or the clip 
